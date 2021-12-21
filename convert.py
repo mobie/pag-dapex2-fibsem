f28e9fc105552c4eb54df2f0556f41811459f99a
Constantin Pape <c.pape@gmx.net>
2021-12-21 14:21:19 +0100

    Convert to new MoBIE spec

---
+++ convert.py
@@ -0,0 +1,6 @@
+from mobie.migration.migrate_v2 import migrate_project
+from mobie.validation import validate_project
+
+# this could also be done via CLI!
+# migrate_project("./data")
+validate_project("./data")

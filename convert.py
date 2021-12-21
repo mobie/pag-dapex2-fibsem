b4fc2fe5b79e42cc6213350e2f568708f2797e1b
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2021-12-21 14:23:55 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,5 +2,5 @@ from mobie.migration.migrate_v2 import migrate_project
 from mobie.validation import validate_project
 
 # this could also be done via CLI!
-# migrate_project("./data")
+migrate_project("./data")
 validate_project("./data")
